9c9201e2fa46cf1e6fde0aed7310fcd958ff4a6e
AmeyaShriwas133 <shriwas.ameya@webseeder.in>
2025-03-12 12:23:47 +0530

    chaat

--- src/Axios/axiosInstance.tsx
+++ src/Axios/axiosInstance.tsx
@@ -2,7 +2,7 @@ import axios, {AxiosInstance} from 'axios'
 
 const accessToken:string | null = localStorage.getItem('accessToken')
 
-const baseURL = 'https://chatappbackend.ameyashriwas.com'
+const baseURL = 'https://api.chatwithus.ameyashriwas.com'
 
 const axiosInstance: AxiosInstance = axios.create({
     baseURL: baseURL as string,
